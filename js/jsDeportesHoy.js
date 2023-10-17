91d82d9b62f7b407ebe6895742b6d0e97aec1fad
patri <avila.patricia.susana@gmail.com>
2023-10-17 12:42:27 -0300

    modifique el footer para hacerlo con JS

---
+++ js/jsDeportesHoy.js
@@ -0,0 +1,134 @@
+
+const scrollElement = document.querySelector(".carousel");
+const scrollLeft = document.querySelector(".scroll-left");
+const scrollRight = document.querySelector(".scroll-right");
+
+const scrollElementvideo = document.querySelector(".carouselvideo");
+const scrollLeftvideo = document.querySelector(".scroll-leftvideo");
+const scrollRightvideo = document.querySelector(".scroll-rightvideo");
+
+function moveScroll(displacement) {
+  let currentPosition = scrollElement.scrollLeft;
+  scrollElement.scrollTo(currentPosition + displacement, 0);
+}
+
+
+
+function moveScrollvideo(displacement) {
+  let currentPosition = scrollElementvideo.scrollLeft;
+  scrollElementvideo.scrollTo(currentPosition + displacement, 0);
+}
+
+scrollLeft.addEventListener("click", () => {
+  moveScroll(-400);
+});
+
+scrollRight.addEventListener("click", () => {
+  moveScroll(400);
+});
+
+scrollLeftvideo.addEventListener("click", () => {
+  moveScrollvideo(-400);
+});
+
+scrollRightvideo.addEventListener("click", () => {
+  moveScrollvideo (400);
+});
+
+// onload = () => {
+//   console.log(scrollElement.style);
+//   if (scrollElement.style.display) {
+//     scrollLeft.style.display = "none";
+//   }
+// };
+
+// onload = () => {
+//   console.log(scrollElementvideo.style);
+//   if (scrollElementvideo.style.display) {
+//     scrollLeftvideo.style.display = "none";
+//   }
+// };
+
+
+
+function ready(cb) {
+  document.addEventListener("DOMContentLoaded", cb);
+}
+
+const linkList = [
+  {
+    url: "index.html",
+    text: "Home"
+  },
+  {
+    url: "https://www.twitter.com",
+    text: "Twitter"
+  },
+  {
+    url: "https://www.linkedin.com",
+    text: "Facebook"
+  },
+  {
+    url: "https://github.com/glrodasz",
+    text: "Instagram"
+  },
+  {
+    url: "https://github.com/glrodasz",
+    text: "Whatsapp"
+  },
+];
+
+function createLink({ text, url }) {
+  // Creamos un elemento tipo link
+  const linkElement = document.createElement("a");
+  // Creamos un nodo de texto
+  const textNode = document.createTextNode(text);
+
+  // Agregamos el texto al link
+  linkElement.appendChild(textNode);
+  // Especificamos la url del link
+  linkElement.href = url;
+  // Especificamos el target
+  linkElement.target = "_blank";
+
+  return linkElement;
+}
+
+function createListItem(linkElement) {
+  // Creamos un elemento tipo item de lista
+  const listItemElement = document.createElement("li");
+
+  // Agregamos nuestro link a nuestro item de lista
+  listItemElement.appendChild(linkElement);
+
+  return listItemElement;
+}
+
+function createList() {
+  // Creamos una elemento tipo lista no-ordenada
+  const unorderedList = document.createElement("ul");
+
+  // Iteramos nuestra lista de links
+  linkList.forEach((link) => {
+    // Creamos un elemento tipo link
+    const linkElement = createLink(link);
+    // Creamos un elemento tipo item de lista
+    const listItemElement = createListItem(linkElement);
+    // Agregamos nuestro item de lista a la lista no-ordenada
+    unorderedList.appendChild(listItemElement);
+  });
+
+  return unorderedList;
+}
+
+function renderFooter() {
+  // Creamos la lista no-ordenada
+  const list = createList();
+  // Seleccionamos el footer
+  const footer = document.querySelector("footer");
+  // Agregamos nuestra lista no-ordenada al footer
+  footer.appendChild(list);
+}
+
+// Cuando nuestro contenido este listo pintamos el footer
+ready(renderFooter);
